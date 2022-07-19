17ce26eedc044b81c30dc828c84328d68242d7ee
SeonminKim1 <yubi5050@naver.com>
2022-07-19 09:11:43 +0900

    Add Utils Code

--- _utils/auto_run_server.py
+++ _utils/auto_run_server.py
@@ -1,4 +1,4 @@
 import os
-os.system('python manage.py makemigrations')
+os.system('python manage.py makemigrations chat project user')
 os.system('python manage.py migrate')
 os.system('python manage.py runserver')
