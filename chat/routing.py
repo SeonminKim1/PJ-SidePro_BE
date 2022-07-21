a3c3073968fa3df18a6d906898e7fdb80dcaae66
SeonminKim1 <yubi5050@naver.com>
2022-07-21 16:32:31 +0900

    Change Room Name UUID #10

--- chat/routing.py
+++ chat/routing.py
@@ -2,5 +2,6 @@ from django.urls import re_path
 from . import consumers
 
 websocket_urlpatterns = [
-    re_path(r'ws/chat/(?P<room_name>\w+)/$', consumers.ChatConsumer.as_asgi()),
+    # re_path(r'ws/chat/(?P<room_name>\w+)/$', consumers.ChatConsumer.as_asgi()), # 일반 ver
+    re_path(r'ws/chat/(?P<room_name>[A-Za-z0-9_-]+)', consumers.ChatConsumer.as_asgi()) # uuid ver
 ]
