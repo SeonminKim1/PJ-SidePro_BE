f275fa5c268bcb30a0b34381b77dac3725afc508
DESKTOP-GBFAR6S\KM <minki.kim@kakao.com>
2022-08-01 19:02:59 +0900

    Fix add dotenv

--- manage.py
+++ manage.py
@@ -2,6 +2,7 @@
 """Django's command-line utility for administrative tasks."""
 import os
 import sys
+import dotenv
 
 
 def main():
@@ -19,4 +20,5 @@ def main():
 
 
 if __name__ == '__main__':
+    dotenv.read_dotenv()
     main()
