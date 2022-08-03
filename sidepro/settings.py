eaf5c0155872cbe362d35f91f41b5949f65a1247
SeonminKim1 <yubi5050@naver.com>
2022-08-03 12:05:31 +0900

    HotFix recommend singleton #30

--- sidepro/settings.py
+++ sidepro/settings.py
@@ -16,8 +16,6 @@ import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
