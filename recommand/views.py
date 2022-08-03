0c34f48af89061ee1cc7d1ff2c9fa1336079a792
SeonminKim1 <yubi5050@naver.com>
2022-08-03 23:59:07 +0900

    Hotfix by new user for recommand

--- recommand/views.py
+++ recommand/views.py
@@ -32,20 +32,25 @@ class RecommendView(APIView):
         if optimize_query:            
             # 3. 자카드 유사도 가장 높은 User N명 출력
             user_id_list, jaccard_score_dict = user_based.get_jaccard_user_id_list(jaccard_score_df, request.user.id)
-
-            # 4. 최종 user들의 project 가져오기
-            project_querysets = Project.objects.select_related('user').prefetch_related('skills', 'bookmark', 'comment_set')\
-                .filter(user__in = user_id_list) # user
-            project_querysets_list = list(project_querysets)
-
-            # # 5. User들의 Project 중 랜덤 추출
-            if len(project_querysets_list) >=3:
-                project_querysets_random3_list = random.sample(project_querysets_list, 3)
-            else: # 3개 미만이면 다시 랜덤 추출
-                project_querysets = Project.objects.select_related('user').prefetch_related('skills', 'bookmark', 'comment_set')\
-                    .exclude(user = request.user.id)
+            # 신규 회원가입 유저
+            if user_id_list == None:
+                project_querysets = Project.objects.select_related('user').prefetch_related('skills', 'bookmark', 'comment_set').all()
                 project_querysets_list = list(project_querysets)
                 project_querysets_random3_list = random.sample(project_querysets_list, 3)
+            else:            
+                # 4. 최종 user들의 project 가져오기
+                project_querysets = Project.objects.select_related('user').prefetch_related('skills', 'bookmark', 'comment_set')\
+                    .filter(user__in = user_id_list) # user
+                project_querysets_list = list(project_querysets)
+
+                # # 5. User들의 Project 중 랜덤 추출
+                if len(project_querysets_list) >=3:
+                    project_querysets_random3_list = random.sample(project_querysets_list, 3)
+                else: # 3개 미만이면 다시 랜덤 추출
+                    project_querysets = Project.objects.select_related('user').prefetch_related('skills', 'bookmark', 'comment_set')\
+                        .exclude(user = request.user.id)
+                    project_querysets_list = list(project_querysets)
+                    project_querysets_random3_list = random.sample(project_querysets_list, 3)
 
             # 6. Serializers 결과 조회.
             rec_result_projects_data = RecommendProjectsSerializer(project_querysets_random3_list, many=True).data
