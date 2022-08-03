8beeedba397c2cedab7893d07890f3fd0a9102f6
Aeius <psb6604@gmail.com>
2022-08-03 11:01:39 +0900

    Add userpage views, serializer, urls

--- user/serializers.py
+++ user/serializers.py
@@ -80,48 +80,11 @@ class SkillsSerializer(serializers.ModelSerializer):
 class AnotherUserSerializer(serializers.ModelSerializer):
     userprofile = UserProfileSerializer()
     user_project = serializers.SerializerMethodField()
-    def get_user_project(self, obj):
-        project_list = []
-        projects = obj.project_set.filter(user__id=obj.pk)
-        for project in projects:
-            data = {
-                "id" : project.id,
-                "title" : project.title,
-                "description" : project.description,
-                "thumnail_img_path" : project.thumnail_img_path,
-                "count" : project.count,
-                "bookmark_count" : project.bookmark_count,
-                "comment_count" : project.comment_count,
-                "skills": skills
-            }
-            project_list.append(data)
-        return project_list
-    
-    # user_bookmark = serializers.SerializerMethodField()
-    # def get_user_bookmark(self, obj):
-    #     project_list = []
-    #     projects = obj.bookmark_set.filter(user__id=obj.pk)
-    #     for project in projects:
-    #         data = {
-    #             "id" : project.id,
-    #             "title" : project.title,
-    #             "description" : project.description,
-    #             "thumnail_img_path" : project.thumnail_img_path,
-    #             "count" : project.count,
-    #             "bookmark_count" : project.bookmark_count,
-    #             "comment_count" : project.comment_count,
-    #         }
-    #         project_list.append(data)
-    #     return project_list
-    
     class Meta:
         model = UserModel
         fields = [
             "id",
             "email",
             "username",
-            "user_project",
-            # "user_bookmark",
-            "userprofile",
             "join_date",
         ]
